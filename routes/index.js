0d12312e75d375f857cc06631c254393d8962325
Clark Sanford <clrksanford@gmail.com>
2018-09-15 10:31:34 -0500

    Flesh out routes

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,8 @@ var mongoose = require('mongoose');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  
+  console.log('hit the endpoint');
+  res.status(200).send();
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var router = express.Router();
+var _ = require('lodash');
+var User = require('../models/users');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* POST user to database. */
+router.post('/', function(req, res, next) {
+  console.log(req.body);
+  req.body = _.pick(req.body, ['email', 'allergens']);
+  console.log(req.body);
+  var user = new User(req.body);
+
+  user.save(user, function (err) {
+    if (err) {
+      res.status(500).send();
+    } else {
+      res.status(204).send();
+    }
+  });
+});
+
+/* DELETE user from database */
+router.delete('/', function (req, res, next) {
+  User.findById(req.params.Id, function (err, user) {
+    if (err) {
+      res.status(500).send();
+    } else if (!user) {
+      res.status(404).send();
+    } else {
+      user.remove(function (err) {
+        if (err) {
+          res.status(500).send();
+        } else {
+          res.status(204).send();
+        }
+      });
+    }
+  });
 });
 
 module.exports = router;
