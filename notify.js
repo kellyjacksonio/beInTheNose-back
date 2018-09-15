f5593801a79c9bfe8f61ff635ac1d5faf92bb8af
Clark Sanford <clrksanford@gmail.com>
2018-09-15 11:16:31 -0500

    Add api call to notify.js, get actual allergens of the day

--- notify.js
+++ notify.js
@@ -2,7 +2,7 @@ require('dotenv').config({silent: true});
 
 var mongoose = require('mongoose');
 var axios = require('axios');
-var _ = require('underscore');
+var _ = require('lodash');
 var User = require('./models/users');
 
 console.log(process.env.DB_CONNECTION);
@@ -18,17 +18,29 @@ db.once('open', function() {
     if(err) {
       console.log(err);
     } else {
-      var highAllergens = ['mold', 'tree'];
-      for (var i = 0; i < users.length; i++) {
-        user = users[i];
-        var allergyIntersect = _.intersection(highAllergens, user.allergens);
-        var hasAllergy = allergyIntersect.length > 0;
+      axios.get('http://dataservice.accuweather.com/forecasts/v1/daily/1day/351193?apikey=qSOlHuy0vvmhUQXyFnc91Q1uv8o4cGcg&details=true')
+        .then(function (res) {
+          var todayAllergens= res.data['DailyForecasts'][0]['AirAndPollen'];
 
-        if(hasAllergy) {
-          // Send email
-          console.log(allergyIntersect);
-        }
-      }
+          var highAllergens = [];
+          todayAllergens.forEach(function (allergen) {
+            if (allergen['Category'] == 'High' || allergen['Category'] == 'Unhealthy' || allergen['Category'] == 'Hazardous') {
+              highAllergens.push(_.toLower(allergen['Name']));
+            };
+          });
+
+          for (var i = 0; i < users.length; i++) {
+            user = users[i];
+            var allergyIntersect = _.intersection(highAllergens, user.allergens);
+            var hasAllergy = allergyIntersect.length > 0;
+
+            if(hasAllergy) {
+              // Send email
+              console.log(allergyIntersect);
+            }
+          }
+
+        })
     }
   })
 })
