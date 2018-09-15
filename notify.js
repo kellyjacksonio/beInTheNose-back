a30602b822e5e5b059323373eebd21f19f48a094
Clark Sanford <clrksanford@gmail.com>
2018-09-15 18:02:01 -0500

    Flesh out notify.js

--- notify.js
+++ notify.js
@@ -1,24 +1,34 @@
 require('dotenv').config({silent: true});
 
 var mongoose = require('mongoose');
-console.log(process.env.DB_CONNECTION);
+var axios = require('axios');
+var _ = require('underscore');
+var User = require('./models/users');
+
 mongoose.connect(process.env.DB_CONNECTION);
+console.log(process.env.DB_CONNECTION);
 
 var db = mongoose.connection;
+db.on('connected', function () {
+    console.log('connected')
+})
 db.on('error', console.error.bind(console, 'connection error'));
 db.once('open', function() {
-  var User = require('./models/users');
-
-  newUser = new User({
-    email: 'clrksanford@gmail.com',
-    allergens: ['mold', 'tree', 'grass', 'ragweed']
-  });
-
-  newUser.save(function(err) {
+  var users = User.find({}, function (err, users) {
     if(err) {
       console.log(err);
     } else {
-      console.log('Success', newUser);
+      var highAllergens = ['mold', 'tree'];
+      for (var i = 0; i < users.length; i++) {
+        user = users[i];
+        var allergyIntersect = _.intersection(highAllergens, user.allergens);
+        var hasAllergy = allergyIntersect.length > 0;
+
+        if(hasAllergy) {
+          // Send email
+          console.log(allergyIntersect);
+        }
+      }
     }
   })
 })
